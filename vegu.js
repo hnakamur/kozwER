a4ece5413ecd3ed21874ef3727f708b303fe918a
Hiroaki Nakamura <hnakamur@gmail.com>
2009-08-30 17:31:42 +0900

    Changed create function also receive setup function.

--- vegu.js
+++ vegu.js
@@ -6,14 +6,40 @@ var vegu = (function() {
     this.elem = elem;
   }
 
-  Manip.createElem = function(type) {
-    return document.createElementNS(svgns, type);
+  Manip.buildElement = function(type, setupFn) {
+    var elem = document.createElementNS(svgns, type);
+    if (setupFn) {
+      setupFn.apply(new Manip(elem),
+          Array.prototype.slice.call(arguments, 2));
+    }
+    return elem;
   };
-  Manip.createFragment = function() {
-    return document.createDocumentFragment(true);
+  Manip.buildFragment = function(setupFn) {
+    var frag = document.createDocumentFragment(true);
+    if (setupFn) {
+      setupFn.apply(new Manip(frag),
+          Array.prototype.slice.call(arguments, 1));
+    }
+    return frag;
   };
 
   Manip.prototype = {
+    appendNewFragment: function(setupFn) {
+      this.elem.appendChild(
+          Manip.buildFragment.apply(undefined, arguments));
+      return this;
+    },
+    appendNewElement: function(type, setupFn) {
+      this.elem.appendChild(
+          Manip.buildElement.apply(undefined, arguments));
+      return this;
+    },
+    setAttrs: function(attrs) {
+      var elem = this.elem;
+      for (var k in attrs)
+        elem.setAttribute(k, attrs[k]);
+      return this;
+    },
     getAttrs: function(names) {
       var attrs = {},
         elem = this.elem;
@@ -22,30 +48,6 @@ var vegu = (function() {
         attrs[name] = elem.getAttribute(name);
       }
       return elems;
-    },
-    setAttrs: function(attrs) {
-      var elem = this.elem;
-      for (var k in attrs)
-        elem.setAttribute(k, attrs[k]);
-      return this;
-    },
-    appendNewElem: function(type, setupFn) {
-      var newElem = Manip.createElem(type);
-      if (setupFn) {
-        setupFn.apply(new Manip(newElem),
-            Array.prototype.slice.call(arguments, 2));
-      }
-      this.elem.appendChild(newElem);
-      return this;
-    },
-    appendNewFragment: function(setupFn) {
-      var frag = Manip.createFragment();
-      if (setupFn) {
-        setupFn.apply(new Manip(frag),
-            Array.prototype.slice.call(arguments, 1));
-      }
-      this.elem.appendChild(frag);
-      return this;
     }
   };
 
