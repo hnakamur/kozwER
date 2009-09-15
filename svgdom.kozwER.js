01ac94574a458be7bbd1de947a25e9a4b2d82ddc
Hiroaki Nakamura <hnakamur@gmail.com>
2009-09-16 01:36:53 +0900

    Updated for svgdom api change.

--- svgdom.kozwER.js
+++ svgdom.kozwER.js
@@ -1,169 +1,15 @@
-svgdom.mixin(svgdom, (function(M) {
-  function curve(pathElements, options) {
-    var config = M.mixin({}, curve.defaults, options);
-    var arrowsOptions = config && config.arrows;
-        arrowAtStart = arrowsOptions && arrowsOptions.start,
-        arrowAtEnd = arrowsOptions && arrowsOptions.end,
-        ret = path = M.path(M.mixin({
-          d: M.formatPath(pathElements)
-        }, M.filterOut(config, 'arrows')));
-
-    if (arrowAtStart || arrowAtEnd) {
-      ret = M.g();
-      ret.append(path);
-      if (path.getTangentToPathAt) {
-        if (arrowAtStart) {
-          var t0 = path.getTangentToPathAt(0);
-          ret.append(arrow(t0.x, t0.y, t0.angle + 180, arrowAtStart));
-        }
-
-        if (arrowAtEnd) {
-          var t1 = path.getTangentToPathAt(1);
-          ret.append(arrow(t1.x, t1.y, t1.angle, arrowAtEnd));
-        }
-      }
-      else {
-        // This is temporary lousy implementation (especially for arc pathSeg)
-        // and should be deleted when SVGPathElement.getTotalLength() and
-        // SVGPathElement.getPointAtLength() are implemented in svgweb.
-        function getControlPointsAbs(commands) {
-          var pts = [],
-              pt = {x: 0, y: 0};
-
-          function nextPoint(relative, x, y) {
-            if (relative) {
-              pt.x += x;
-              pt.y += y;
-            }
-            else {
-              pt.x = x;
-              pt.y = y;
-            }
-            pts.push({x: pt.x, y: pt.y});
-          }
-
-          for (var i = 0, n = commands.length; i < n; i++) {
-            var command = commands[i],
-                cmdChar = command[0],
-                relative = (cmdChar === cmdChar.toLowerCase()),
-                m = command.length,
-                j = 1,
-                paramCount = m - j,
-                x, y;
-            switch (cmdChar.toUpperCase()) {
-            case 'M':
-            case 'L':
-            case 'T':
-              while (j < m) {
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-              }
-              break;
-            case 'S':
-            case 'Q':
-              while (j < m) {
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-              }
-              break;
-            case 'C':
-              while (j < m) {
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-              }
-              break;
-            case 'H':
-              while (j < m) {
-                x = command[j++];
-                nextPoint(relative, x, 0);
-              }
-              break;
-            case 'V':
-              while (j < m) {
-                y = command[j++];
-                nextPoint(relative, 0, y);
-              }
-              break;
-            case 'Z':
-              break;
-            case 'A':
-              while (j < m) {
-                j += 5;
-                x = command[j++];
-                y = command[j++];
-                nextPoint(relative, x, y);
-              }
-              break;
-            default:
-              throw new Error('Unsupported path command. command=' + cmdChar);
-            }
-          }
-          return pts;
-        }
-
-        var pts = getControlPointsAbs(pathElements);
-        if (arrowAtStart) {
-          var p0 = pts[0],
-              p1 = pts[1],
-              angle = M.geom.rad2deg(Math.atan2(p0.y - p1.y, p0.x - p1.x));
-          ret.append(arrow(p0.x, p0.y, angle, arrowAtStart));
-        }
-
-        if (arrowAtEnd) {
-          var n = pts.length,
-              p0 = pts[n - 2],
-              p1 = pts[n - 1],
-              angle = M.geom.rad2deg(Math.atan2(p1.y - p0.y, p1.x - p0.x));
-          ret.append(arrow(p1.x, p1.y, angle, arrowAtEnd));
-        }
-      }
-    }
-    return ret;
-  }
-  curve.defaults = {
-    'class': 'curve',
-    stroke: '#000',
-    fill: 'none'
-  };
-
-  function arrow(x, y, angle, options) {
-    var config = M.mixin({}, arrow.defaults, options);
-    var w = config.arrowLength;
-    var h = w * Math.tan(M.geom.deg2rad(config.arrowAngle / 2));
-    return M.path().setAttr(M.mixin({
-      d: M.formatPath([
-        ['M', 0, 0],
-        ['l', -w, h, 0, -h * 2],
-        ['z']
-      ]),
-      transform: M.rotateThenTranslateTransform(x, y, angle)
-    }, M.filterOut(config, 'arrowAngle', 'arrowLength')));
-  }
-  arrow.defaults = {
-    'class': 'arrow',
-    stroke: '#000',
-    fill: 'none',
-    arrowAngle: 45,
-    arrowLength: 10
-  };
+svgdom.mixin(svgdom.Element.prototype, (function() {
+  var mixin = svgdom.mixin,
+      filterOut = svgdom.filterOut,
+      geom = svgdom.geom,
+      rad2deg = geom.rad2deg,
+      deg2rad = geom.deg2rad;
 
   function relationLine(end1, end2, options) {
-    var config = M.mixin({}, relationLine.defaults, options);
-    var end1 = relationEnd(end1.x, end1.y, end1.angle, end1.cardinarity,
+    var config = mixin({}, relationLine.defaults, options);
+    var end1 = this.relationEnd(end1.x, end1.y, end1.angle, end1.cardinarity,
         options.end1);
-    var end2 = relationEnd(end2.x, end2.y, end2.angle, end2.cardinarity,
+    var end2 = this.relationEnd(end2.x, end2.y, end2.angle, end2.cardinarity,
         options.end2);
     var p1, p2;
 
@@ -192,19 +38,19 @@ svgdom.mixin(svgdom, (function(M) {
   };
 
   function relationEnd(x, y, angle, cardinality, options) {
-    var config = M.mixin({}, relationEnd.defaults, options);
+    var config = mixin({}, relationEnd.defaults, options);
     var h = config.relationEndLength;
     var w = h * config.relationEndWidthRatio;
-    var transform = M.rotateThenTranslateTransform(x, y, angle);
+    var transform = this.rotateThenTranslateTransform(x, y, angle);
     var elem;
     switch (cardinality) {
     case 'one':
       var x2 = h * config.oneConnectorEndPosRatio;
-      elem = M.path({
+      elem = this.path({
         'class': 'relationEnd one',
         stroke: '#000',
         fill: 'none',
-        d: M.formatPath([
+        d: this.formatPath([
           ['M', 0, 0],
           ['L', -h, 0],
           ['M', -x2, -w / 2],
@@ -215,11 +61,11 @@ svgdom.mixin(svgdom, (function(M) {
       break;
     case 'many':
       var r = w / 2;
-      elem = M.path({
+      elem = this.path({
         'class': 'relationEnd many',
         stroke: '#000',
         fill: 'none',
-        d: M.formatPath([
+        d: this.formatPath([
           ['M', 0, 0],
           ['L', -h, 0],
           ['M', 0, -r],
@@ -238,32 +84,32 @@ svgdom.mixin(svgdom, (function(M) {
         pathElems.push(['M', -dotLen * 2 * i, 0]);
         pathElems.push(['L', -dotLen * (2 * i + 1), 0]);
       }
-      elem = M.path({
+      elem = this.path({
         'class': 'relationEnd ref',
         stroke: '#000',
         fill: 'none',
-        d: M.formatPath(pathElems),
+        d: this.formatPath(pathElems),
         transform: transform
       });
       break;
     case 'inherit':
       var x2 = h * config.oneConnectorEndPosRatio;
       var r = h * config.inheritConnectorEndRadiusRatio;
-      elem = M.g({
+      elem = this.g({
         'class': 'relationEnd inherit',
         transform: transform
       }).append(
-        M.path({
+        this.path({
           stroke: '#000',
           fill: 'none',
-          d: M.formatPath([
+          d: this.formatPath([
             ['M', 0, 0],
             ['L', -h, 0],
             ['M', -x2, -w / 2],
             ['L', -x2, w / 2]
           ])
         }),
-        M.circle({
+        this.circle({
           cx: -h, cy: 0, r: r, fill: '#000'
         })
       );
@@ -282,9 +128,7 @@ svgdom.mixin(svgdom, (function(M) {
   };
 
   return {
-    curve: curve,
-    arrow: arrow,
     relationLine: relationLine,
     relationEnd: relationEnd
   };
-})(svgdom));
+})());
