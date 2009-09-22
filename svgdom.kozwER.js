cfba955fd308a77ed7ca543b47c2c243af2f6515
Hiroaki Nakamura <hnakamur@gmail.com>
2009-09-22 14:26:17 +0900

    Added erDiagram.

--- svgdom.kozwER.js
+++ svgdom.kozwER.js
@@ -5,14 +5,134 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
       rad2deg = geom.rad2deg,
       deg2rad = geom.deg2rad;
 
+  function erDiagram(settings) {
+    var config = buildErDiagramConfig(settings);
+    var tableConfigs = config.tables;
+    var tableCount = tableConfigs.length;
+
+    var x = 0;
+    var y = 0;
+    var gErDiagram = this.g({'class': 'erDiagram'});
+    for (var i = 0; i < tableCount; i++) {
+      var tableConfig = tableConfigs[i];
+      var margin = tableConfig.margin;
+      x = margin.x;
+      y += margin.y;
+      var tableElem = table.call(gErDiagram, x, y, tableConfig.name, {
+        id: tableConfig.id,
+        columns: tableConfig.columns
+      });
+      config.tableHash[tableConfig.id].nodeWrapper = tableElem;
+      var tableRect = tableElem.boundingBox;
+      y += tableRect.height;
+    }
+
+    var connectorConfigs = config.connectors;
+    var connectorCount = connectorConfigs.length;
+    for (var i = 0; i < connectorCount; i++) {
+      var connectorConfig = connectorConfigs[i];
+      var endsConfig = connectorConfig.ends;
+      for (var j = 0, endCount = endsConfig.length; j < endCount; j++) {
+        var endConfig = endsConfig[j];
+        var p = connectorEndPoint(config, endConfig);
+        endConfig.x = p.x;
+        endConfig.y = p.y;
+        if (endConfig.side === 'left')
+          endConfig.angle = 0;
+        else if (endConfig.side === 'top')
+          endConfig.angle = 90;
+        else if (endConfig.side === 'bottom')
+          endConfig.angle = 270;
+      }
+      relationLine.call(gErDiagram, endsConfig[0], endsConfig[1]);
+    }
+
+    gErDiagram.config = config;
+    console.log(gErDiagram);
+    return gErDiagram;
+  }
+  erDiagram.defaults = {
+    connectorEndLen: 10,
+    connectorWidthRatio: 0.6,
+    connectorMarginRatio: 0.3,
+    connectorEndFirstOffset: 8,
+    oneConnectorEndPosRatio: 0.4,
+    inheritConnectorEndRadiusRatio: 0.3,
+    inheritConnectorOffsetX: 20
+  };
+
+  function buildErDiagramConfig(settings) {
+    var config = mixin({}, erDiagram.defaults, settings);
+    var tableConfigs = config.tables;
+    var tableCount = tableConfigs.length;
+
+    config.tableHash = {};
+    for (var i = 0; i < tableCount; i++) {
+      var tableConfig = tableConfigs[i];
+      config.tableHash[tableConfig.id] = tableConfig;
+    }
+
+    var connectorConfigs = config.connectors;
+    var connectorCount = connectorConfigs.length;
+    for (var i = 0; i < connectorCount; i++) {
+      var connectorConfig = connectorConfigs[i];
+      var endsConfig = connectorConfig.ends;
+      for (var j = 0, endCount = endsConfig.length; j < endCount; j++) {
+        var endConfig = endsConfig[j];
+        var tableId = endConfig.table;
+        var tableConfig = config.tableHash[tableId];
+        if (tableConfig.ends === undefined)
+          tableConfig.ends = {top: [], left: [], bottom: []};
+        var side = endConfig.side;
+        if (!(side === 'top' || side === 'bottom' || side == 'left'))
+          throw new Error('Invalid endConfig.side');
+        var endsAtSide = tableConfig.ends[side];
+        if (endConfig.index === undefined)
+          endConfig.index = endsAtSide.length;
+        endsAtSide[endConfig.index] = endConfig;
+      }
+    }
+    return config;
+  }
+
+  function connectorEndPoint(erDiagramConfig, endConfig) {
+    var tableConfig = erDiagramConfig.tableHash[endConfig.table],
+        tableElem = tableConfig.nodeWrapper,
+        box = tableElem.nameBox,
+        cew = erDiagramConfig.connectorEndLen * erDiagramConfig.connectorWidthRatio,
+        cem = erDiagramConfig.connectorEndLen * erDiagramConfig.connectorMarginRatio,
+        x, y;
+    switch (endConfig.side) {
+    case 'top':
+      x = box.x + erDiagramConfig.connectorEndFirstOffset +
+          endConfig.index * (cew + cem);
+      y = box.y;
+      break;
+    case 'bottom':
+      x = box.x + erDiagramConfig.connectorEndFirstOffset +
+          endConfig.index * (cew + cem);
+      y = box.y + box.height;
+      break;
+    case 'left':
+      x = box.x;
+      var c = tableConfig.ends[endConfig.side].length,
+          w = cew * c + cem * (c - 1);
+      y = box.y + box.height / 2 - w / 2 +
+          endConfig.index * (cew + cem) + cew / 2;
+      break;
+    }
+    return {x: x, y: y};
+  }
 
   function table(x, y, name, options) {
     var config = mixin({}, table.defaults, options);
     var gTable = this.g({'class': 'table'});
 
     var gName = gTable.g({'class': 'tableName'});
-    var rect = gName.rect({x: x, y: y, width: 1, height: 1,
-        stroke: '#000', fill: 'none'});
+    var rect = gName.rect({
+      x: x, y: y, width: 1, height: 1,
+      stroke: '#000', fill: 'none'
+    });
     var xName = x + config.nameBoxPadding,
         yName = y + config.nameBoxPadding;
     var text = gName.text({
@@ -25,6 +145,7 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
 
     var nameBox = gTable.nameBox =
         geom.insetRect(text.getTextBBox(), -config.nameBoxPadding);
+    gTable.boundingBox = geom.cloneRect(nameBox);
     rect.setAttr({width: nameBox.width, height: nameBox.height});
 
     var gColumns = gTable.g({'class': 'columns'});
@@ -61,6 +182,7 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
           x += config.columnMargin;
         }
       }
+      gTable.boundingBox.width = x - nameBox.x;
     }
 
     return gTable;
@@ -73,12 +195,15 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
   }
 
   function relationLine(end1, end2, options) {
+    if (end1.y > end2.y)
+      return relationLine(end2, end1, options);
+
     var config = mixin({}, relationLine.defaults, options);
-    var end1 = this.relationEnd(end1.x, end1.y, end1.angle, end1.cardinarity,
-        options.end1);
-    var end2 = this.relationEnd(end2.x, end2.y, end2.angle, end2.cardinarity,
-        options.end2);
-    var p1, p2;
+    var gRelationLine = this.g({'class': config['class']});
+    var end1Elem = relationEnd.call(gRelationLine, end1.x, end1.y, end1.angle,
+        end1.cardinality, options && options.end1);
+    var end2Elem = relationEnd.call(gRelationLine, end2.x, end2.y, end2.angle,
+        end2.cardinality, options && options.end2);
 
     function endStartPoint(x, y, angle, length) {
       var rad = deg2rad(angle);
@@ -88,18 +213,103 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
       };
     }
 
-    if (end1.y < end2.y) {
-      p1 = endStartPoint(end1.x, end1.y, end1.angle);
-      p2 = endStartPoint(end2.x, end2.y, end2.angle);
-    }
-    else {
-      p1 = endStartPoint(end2.x, end2.y, end2.angle);
-      p2 = endStartPoint(end1.x, end1.y, end1.angle);
+    var p1, p2;
+    p1 = endStartPoint(end1.x, end1.y, end1.angle, config.relationEndLength);
+    p2 = endStartPoint(end2.x, end2.y, end2.angle, config.relationEndLength);
+    var r = config.connectorCornerRadius;
+    switch (end1.side) {
+    case 'top':
+      switch (end2.side) {
+      case 'top':
+        break;
+      case 'bottom':
+        break;
+      case 'left':
+        gRelationLine.path({
+          d: this.formatPath([
+            ["M", p1.x, p1.y],
+            ["L", p1.x, p2.y - r],
+            ["A", r, r, 0, 0, 1, p1.x + r, p2.y],
+            ["L", p2.x, p2.y]
+          ]),
+          stroke: config.stroke,
+          fill: config.fill
+        });
+        break;
+      }
+      break;
+    case 'bottom':
+      switch (end2.side) {
+      case 'top':
+        break;
+      case 'bottom':
+        break;
+      case 'left':
+        gRelationLine.path({
+          d: this.formatPath([
+            ["M", p1.x, p1.y],
+            ["L", p1.x, p2.y - r],
+            ["A", r, r, 0, 0, 0, p1.x + r, p2.y],
+            ["L", p2.x, p2.y]
+          ]),
+          stroke: config.stroke,
+          fill: config.fill
+        });
+        break;
+      }
+      break;
+    case 'left':
+      switch (end2.side) {
+      case 'top':
+        gRelationLine.path({
+          d: this.formatPath([
+            ["M", p2.x, p2.y],
+            ["L", p2.x, p1.y + r],
+            ["A", r, r, 0, 0, 1, p2.x + r, p1.y],
+            ["L", p1.x, p1.y]
+          ]),
+          stroke: config.stroke,
+          fill: config.fill
+        });
+        break;
+      case 'bottom':
+        gRelationLine.path({
+          d: this.formatPath([
+            ["M", p2.x, p2.y],
+            ["L", p2.x, p1.y - r],
+            ["A", r, r, 0, 0, 0, p2.x + r, p1.y],
+            ["L", p1.x, p1.y]
+          ]),
+          stroke: config.stroke,
+          fill: config.fill
+        });
+        break;
+      case 'left':
+        var x = p1.x - config.inheritConnectorOffsetX;
+        gRelationLine.path({
+          d: this.formatPath([
+            ["M", p1.x, p1.y],
+            ["L", x + r, p1.y],
+            ["A", r, r, 0, 0, 0, x, p1.y + r],
+            ["L", x, p2.y - r],
+            ["A", r, r, 0, 0, 0, x + r, p2.y],
+            ["L", p2.x + r, p2.y]
+          ]),
+          stroke: config.stroke,
+          fill: config.fill
+        });
+        break;
+      }
+      break;
     }
 
+    return gRelationLine;
   }
   relationLine.defaults = {
     'class': 'relationLine',
+    relationEndLength: 10,
+    connectorCornerRadius: 10,
+    inheritConnectorOffsetX: 20,
     stroke: '#000',
     fill: 'none'
   };
@@ -195,8 +405,8 @@ svgdom.mixin(svgdom.Element.prototype, (function() {
   };
 
   return {
-    table: table,
-    relationLine: relationLine,
-    relationEnd: relationEnd
+    kozwER: {
+      erDiagram: erDiagram
+    }
   };
 })());
